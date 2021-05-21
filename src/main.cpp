b2d4d9c177bf2b29f7854fa79df0d91448e89826
kajuha <kajuha@gmail.com>
2021-05-22 01:20:41 +0900

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,34 @@ using Eigen::MatrixXd;
 
 #include <string>
 
+class ClassTest {
+    public:
+    // short *array;
+    std::vector<short> array;
+
+    public:
+    ClassTest(int num) {
+        std::cout << "construct class : " << array.size() << std::endl;
+        array.push_back(1);
+        array.push_back(2);
+        array.push_back(3);
+        std::cout << "allocate array: " << array.size() << std::endl;
+    }
+
+    ClassTest(int num, int abc) {
+        std::cout << "construct class : " << array.size() << std::endl;
+        array = std::vector<short>(num);
+        array[0] = 3;
+        array[1] = 4;
+        array[2] = 5;
+        std::cout << "allocate array: " << array.size() << std::endl;
+    }
+
+    ~ClassTest() {
+        std::cout << "destruct array: " << array.size() << std::endl;
+    }
+};
+
 void callBackInt(const dummy_cpp::msg_int::ConstPtr& vel) {
 }
 
@@ -22,24 +50,41 @@ void fThread(int* thread_rate) {
     ros::NodeHandlePtr node = boost::make_shared<ros::NodeHandle>();
     ros::Rate rate(*thread_rate);
 
-    while (ros::ok())
-    {
-        rate.sleep();
-    }
+    // while (ros::ok())
+    // {
+    //     rate.sleep();
+    // }
 }
 
 int main(int argc, char** argv)
 {
+    ClassTest test1(11);
+    test1 = ClassTest(11, 11);
+
+    for (short x: test1.array) {
+        std::cout << "indexing: " << x << std::endl;
+    }
+    // ClassTest test2(11, 11);
+    // other library include test
     MatrixXd a(2, 2);
     MatrixXd b(2, 2);
     MatrixXd c(2, 2);
     a << 1, 0, 0, 1;
     b << 1, 2, 3, 4;
     c = a * b;
-    std::cout << c << std::endl;
+    // std::cout << c << std::endl;
 
     ros::init(argc, argv, "dummy_cpp");
     ros::NodeHandle nh("~");
+
+    std::vector<std::string> keys;
+    // nh.getParamNames(keys);
+    // std::cout << "getParamNames : " << std::endl;
+    // for (int i=0; i<keys.size(); i++) {
+    //     std::cout << keys[i];
+    //     std::cout << "\n";
+    // }
+    // std::cout << std::endl;
     
     ros::Subscriber test1_sub = nh.subscribe("test1_sub_topic", 100, callBackInt);
     ros::Subscriber test2_sub = nh.subscribe("test2_sub_topic", 100, callBackFloat);
@@ -64,21 +109,21 @@ int main(int argc, char** argv)
     double time_pre = time_cur;
     double time_diff;
 
-    while(ros::ok())
-    {
-        time_cur = ros::Time::now().toSec();
-        time_diff = time_cur - time_pre;
-#define PERIOD  0.1
-        if ( time_diff > PERIOD) {
-            time_pre = time_cur;
-        }
+//     while(ros::ok())
+//     {
+//         time_cur = ros::Time::now().toSec();
+//         time_diff = time_cur - time_pre;
+// #define PERIOD  0.1
+//         if ( time_diff > PERIOD) {
+//             time_pre = time_cur;
+//         }
 
-        printf("timd_diff: %lf\n", time_diff);
+//         // printf("timd_diff: %lf\n", time_diff);
 
-        ros::spinOnce();
+//         ros::spinOnce();
 
-        main_rate.sleep();
-    }
+//         main_rate.sleep();
+//     }
 
     hThread.join();
 }
